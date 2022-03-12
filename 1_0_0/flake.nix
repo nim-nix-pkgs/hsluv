c964975cdea73df19325df4f30b6e22ad6c97283
hugosenari <hugosenari@gmail.com>
2022-03-12 03:38:59 -0300

    chore: re index hsluv

---
+++ 1_0_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A port of HSLuv, a human friendly alternative to HSL.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-hsluv-1_0_0.flake = false;
+  inputs.src-hsluv-1_0_0.owner = "isthisnagee";
+  inputs.src-hsluv-1_0_0.ref   = "refs/tags/1.0.0";
+  inputs.src-hsluv-1_0_0.repo  = "hsluv-nim";
+  inputs.src-hsluv-1_0_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-hsluv-1_0_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-hsluv-1_0_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A port of HSLuv, a human friendly alternative to HSL.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-hsluv-master.flake = false;
+  inputs.src-hsluv-master.owner = "isthisnagee";
+  inputs.src-hsluv-master.ref   = "refs/heads/master";
+  inputs.src-hsluv-master.repo  = "hsluv-nim";
+  inputs.src-hsluv-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-hsluv-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-hsluv-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
